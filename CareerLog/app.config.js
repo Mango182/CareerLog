6110afadfaec479e711a13dd5cb607a1315e0a21
Mango182 <Miguel97Echeverry76@gmail.com>
2026-05-30 22:19:09 -0400

    feat: add app configuration for development and production environments

---
+++ CareerLog/app.config.js
@@ -0,0 +1,19 @@
+export default ({ config }) => {
+  const isDev = process.env.APP_ENV === "development";
+  return {
+    ...config,
+    name: isDev ? "CareerLog Dev" : "CareerLog",
+    ios: {
+      ...config.ios,
+      bundleIdentifier: isDev
+        ? "com.mango1823192.CareerLog.dev"
+        : "com.mango1823192.CareerLog",
+    },
+    android: {
+      ...config.android,
+      package: isDev
+        ? "com.mango1823192.CareerLog.dev"
+        : "com.mango1823192.CareerLog",
+    },
+  };
+};
