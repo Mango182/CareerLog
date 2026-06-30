78917518270671797e17ef61de1cbbf076f2e98e
Mango182 <Miguel97Echeverry76@gmail.com>
2026-06-30 18:50:51 -0400

    feat: added profile and login to navbar

--- CareerLog/app/(tabs)/_layout.tsx
+++ CareerLog/app/(tabs)/_layout.tsx
@@ -1,33 +1,23 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import React from 'react';
-import { Pressable } from 'react-native';
+import { NativeTabs } from 'expo-router/unstable-native-tabs';
 
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-import { useTheme } from '@/constants/useThemes';
-
-import { NativeTabs } from 'expo-router/unstable-native-tabs'
-import { Platform } from 'react-native';
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+import { useAuth } from '@/context/AuthenticationContext';
 
 export default function TabLayout() {
-  const { colors } = useTheme();
-  // if (Platform.OS === 'ios') {
+  const { user } = useAuth();
+
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="index">
         <NativeTabs.Trigger.Icon sf="house.fill" md="home" />
+        <NativeTabs.Trigger.Label>Dashboard</NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="applications">
         <NativeTabs.Trigger.Icon sf="briefcase.fill" md="work" />
         <NativeTabs.Trigger.Label>Applications</NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name={user ? 'profile' : 'login'}>
+        <NativeTabs.Trigger.Icon sf="person.circle.fill" md="person" />
+        <NativeTabs.Trigger.Label>{user ? 'Profile' : 'Login'}</NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
     </NativeTabs>
   );
